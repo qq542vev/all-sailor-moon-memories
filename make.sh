08b04b023dcfa4a1801d764464e3856ac402ab12
qq542vev <qq542vev@yahoo.co.jp>
2023-04-10 09:18:19 +0900

    初めての commit

---
+++ make.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env sh
+
+### Script: make.sh
+##
+## All Sailor Moon Memories の動画を作成する。
+##
+## Metadata:
+##
+##   id - 69fc451a-d276-4fc4-9f7c-cff01b99319b
+##   author - <qq542vev at https://purl.org/meta/me/>
+##   version - 1.0.0
+##   date - 2023-04-09
+##   since - 2023-04-09
+##   copyright - Copyright (C) 2023-2023 qq542vev. Some rights reserved.
+##   license - <CC-BY at https://creativecommons.org/licenses/by/4.0/>
+##   package - all-sailor-moon-memories
+##
+## See Also:
+##
+##   * <Project homepage at https://github.com/qq542vev/all-sailor-moon-memories>
+##   * <Bag report at https://github.com/qq542vev/all-sailor-moon-memories/issues>
+
+set -efu
+umask '0022'
+readonly "LC_ALL_ORG=${LC_ALL-}"
+LC_ALL='C'
+IFS=$(printf ' \t\n_'); IFS="${IFS%_}"
+PATH="${PATH-}${PATH:+:}$(command -p getconf 'PATH')"
+UNIX_STD='2003' # HP-UX POSIX mode
+XPG_SUS_ENV='ON' # AIX POSIX mode
+XPG_UNIX98='OFF' # AIX UNIX 03 mode
+POSIXLY_CORRECT='1' # GNU Coreutils POSIX mode
+COMMAND_MODE='unix2003' # macOS UNIX 03 mode
+export 'LC_ALL' 'IFS' 'PATH' 'UNIX_STD' 'XPG_SUS_ENV' 'XPG_UNIX98' 'POSIXLY_CORRECT' 'COMMAND_MODE'
+
+readonly 'downloads=
+youtube:cBRYceV7b1Q=https://www.youtube.com/watch?v=cBRYceV7b1Q
+youtube:hj_xSv0F76Q=https://www.youtube.com/watch?v=hj_xSv0F76Q
+youtube:coShQEyM0ic=https://www.youtube.com/watch?v=coShQEyM0ic
+'
+readonly 'options=--abort-on-error --continue --ignore-config --no-cache-dir --retries 100'
+readonly 'format=%(extractor)s:%(id)s.%(format_id)s.%(ext)s'
+
+mkdir -p -- 'videos' 'audios'
+
+for line in ${downloads}; do
+	name="${line%%=*}"
+	url="${line#*=}"
+
+	if [ '!' -f "videos/${name}" ]; then
+		yt-dlp ${options} --format 'bestvideo[width=640][height=480]' --output "videos/${format}" -- "${url}"
+		rm -fr -- "videos/${name}"
+		ln -s -- "$(yt-dlp ${options} --format 'bestvideo[width=640][height=480]' --output "${format}" --get-filename -- "${url}")" "videos/${name}"
+	fi
+
+	if [ '!' -f "audios/${name}" ]; then
+		yt-dlp ${options} --format 'bestaudio' --output "audios/${format}" -- "${url}"
+		rm -fr -- "audios/${name}"
+		ln -s -- "$(yt-dlp ${options} --format 'bestaudio' --output "${format}" --get-filename -- "${url}")" "audios/${name}"
+	fi
+
+	videos="${videos-} -i videos/${name}"
+	audios="${audios-} -i audios/${name}"
+done
+
+ffmpeg ${videos} ${audios} \
+	-filter_complex '
+		color=s=1280x960:c=black[background];
+ 		[1:v]trim=start=02.16666667:end=01\\:34.2,setpts=PTS-STARTPTS+0.3/TB[trimmed_v];
+		[4:a]atrim=start=02.16666667:end=01\\:34.2,asetpts=PTS-STARTPTS+0.3/TB[trimmed_a];
+		[1:v]trim=start=00:end=02,setpts=PTS-STARTPTS+7/TB[logo];
+		[background][0:v]overlay=x=0:y=0[1video];
+		[1video][trimmed_v]overlay=x=640:y=0[2video];
+		[2video][2:v]overlay=x=0:y=480[3video];
+		[3video][logo]overlay=x=640:y=480
+	' \
+	-map '3:a' -map '[trimmed_a]' -map '5:a' \
+	-c:v libx264 -r 30 -fps_mode cfr -crf 0 -qp 0 -preset placebo -tune animation \
+	-c:a flac -ar 48000 -ac 2 -compression_level 12 \
+	-metadata 'title=All Sailor Moon Memories' \
+	-metadata:s:a:0 'language=jpn' \
+	-metadata:s:a:0 'title=sailor moon memories opening 2nd version セーラームーン' \
+	-metadata:s:a:0 'description=Source: https://www.youtube.com/watch?v=cBRYceV7b1Q' \
+	-metadata:s:a:1 'language=spa' \
+	-metadata:s:a:1 'title=Opening sailor moon MemorieS' \
+	-metadata:s:a:1 'description=Source: https://www.youtube.com/watch?v=hj_xSv0F76Q' \
+	-metadata:s:a:2 'language=spa' \
+	-metadata:s:a:2 'title=sailor moon memories opening' \
+	-metadata:s:a:2 'description=Source: https://www.youtube.com/watch?v=coShQEyM0ic' \
+	-to '01:33' all-sailor-moon-memories.mkv
---
+++ readme.md
@@ -0,0 +1,38 @@
+<!-- Document: readme.md
+
+	All Sailor Moon Memories のマニュアル
+
+	Metadata:
+
+		id - 4d5beda3-572f-471f-8c96-67e994c385b9
+		author - <qq542vev at https://purl.org/meta/me/>
+		version - 1.0.0
+		date - 2023-04-09
+		since - 2023-04-09
+		copyright - Copyright (C) 2023-2023 qq542vev. Some rights reserved.
+		license - <CC-BY at https://creativecommons.org/licenses/by/4.0/>
+		package - all-sailor-moon-memories
+
+	See Also:
+
+		* <Project homepage at https://github.com/qq542vev/all-sailor-moon-memories>
+		* <Bag report at https://github.com/qq542vev/all-sailor-moon-memories/issues>
+-->
+
+# All Sailor Moon Memories
+
+All Sailor Moon Memories は Sailor Moon Memories の各動画を一覧表示した動画を作成する試みです。Sailor Moon Memoriesは [fabzt10] (https://www.youtube.com/channel/UCi30Verb3Spu3oQiKnlmsqg) さんによって作成されたセーラームーンを題材にした幾つかの AMV(Anime Music Video) です。
+
+現状では以下の３つの動画を分割して一覧表示します。
+
+ * 左上 - [sailor moon memories opening 2nd version セーラームーン](https://www.youtube.com/watch?v=cBRYceV7b1Q)
+ * 右上 - [Opening sailor moon MemorieS](https://www.youtube.com/watch?v=hj_xSv0F76Q)
+ * 左下 - [sailor moon memories opening](https://www.youtube.com/watch?v=coShQEyM0ic)
+
+# 動画を作成する
+
+動画を作成するにはご使用のコンピューターに [yt-dlp](https://github.com/yt-dlp/yt-dlp) と [FFmpeg](https://ffmpeg.org/) がインストールされている必要があります。可能な限り最新版を使用してください。2023年04月19日現在、yt-dlp は [2023.03.04](https://github.com/yt-dlp/yt-dlp/releases/tag/2023.03.04)、FFmpeg は [5.1.1-static](https://ffmpeg.org/download.html) での組み合わせで動作することを確認しています。
+
+[`make.sh`](make.sh) をダウンロードして、実行してください。YouTube から動画をダウンロードします。ダウンロードされた動画はカレントディレクトリの `./vidoes`, `./audios` 内に保存されます。その後、動画の作成を開始します。動画の作成(主にエンコード)には非常に時間がかかります。
+
+作成された動画はカレントディレクトリ内の `all-sailor-moon-memories.mkv` として保存されます。作成された動画の構成は、Matroska Video + H.264 + FLAC(3トラック)です。詳細は Mediainfo の[出力結果](all-sailor-moon-memories.mkv.mediainfo.txt)を参照してください。
